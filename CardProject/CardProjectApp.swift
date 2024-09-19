377a502e79c3597330cfe3211c050bd1393a62d4
RMCSa <rafamoca42@gmail.com>
2024-09-19 16:28:44 -0300

    Initial Commit

---
+++ CardProject/CardProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardProjectApp.swift
+//  CardProject
+//
+//  Created by RAFAEL MOREIRA CAVALCANTE DE SOUZA on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CardProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardProject
+//
+//  Created by RAFAEL MOREIRA CAVALCANTE DE SOUZA on 19/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
